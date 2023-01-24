b580f02c0a16eefa5010ed5ca1b5c8c7c7b2214f
Rahmat Cristoper Sinambela <rahmatcs@hanabank.co.id>
2023-01-24 09:21:54 +0700

    using dependency injection with abstraction

--- src/main/java/org/example/configuration/ProjectConfiguration.java
+++ src/main/java/org/example/configuration/ProjectConfiguration.java
@@ -1,13 +1,36 @@
 package org.example.configuration;
 
+import org.example.proxies.CommentNotificationProxy;
+import org.example.proxies.EmailCommentNotificationProxy;
+import org.example.repositories.CommentRepository;
+import org.example.repositories.DBCommentRepository;
+import org.example.services.CommentService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {
-        "org.example.proxies"
-        ,"org.example.services",
-        "org.example.repositories"
-})
+//@ComponentScan(basePackages = {
+//        "org.example.proxies"
+//        ,"org.example.services",
+//        "org.example.repositories"
+//})
 public class ProjectConfiguration {
+
+    @Bean
+    public CommentRepository commentRepository() {
+        return  new DBCommentRepository();
+    }
+
+    @Bean
+    public CommentNotificationProxy commentNotificationProxy() {
+        return  new EmailCommentNotificationProxy();
+    }
+
+    @Bean
+    public CommentService commentService(
+            CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy
+    ) {
+        return new CommentService(commentRepository,commentNotificationProxy);
+    }
 }
--- src/main/java/org/example/services/CommentService.java
+++ src/main/java/org/example/services/CommentService.java
@@ -3,18 +3,27 @@ package org.example.services;
 import org.example.model.Comment;
 import org.example.proxies.CommentNotificationProxy;
 import org.example.repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentService {
-    private final CommentRepository commentRepository;
-    private final CommentNotificationProxy commentNotificationProxy;
-
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private CommentNotificationProxy commentNotificationProxy;
 
     public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
-        this.commentRepository = commentRepository;
-        this.commentNotificationProxy = commentNotificationProxy;
     }
+
+    /*
+    * This is used for constructor injection
+    * switch using @autowired
+    * */
+//    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+//        this.commentRepository = commentRepository;
+//        this.commentNotificationProxy = commentNotificationProxy;
+//    }
 
     public void publishComment(Comment comment) {
         commentRepository.storeComment(comment);
